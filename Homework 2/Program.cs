431fc5bd5a2f1009503a15535d148f0b8847fa6d
Paradoru <LicLexa@yandex.ru>
2021-10-20 16:07:25 +0200

    Добавьте файлы проекта.

---
+++ Homework 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите x:");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите y:");
+                double y = double.Parse(Console.ReadLine());
+                if (x>=-1 && x<=1 && y>=0.5 & y<=1)
+                    Console.WriteLine("Да");
+                else
+                    Console.WriteLine("Нет");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
